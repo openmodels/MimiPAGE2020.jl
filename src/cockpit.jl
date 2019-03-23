b9e827ec513fd2162deddbe5ca26abefe5d33452
James Rising <jarising@gmail.com>
2019-03-23 18:05:47 +0000

    add cockpit

---
+++ src/cockpit.jl
@@ -0,0 +1,13 @@
+scenariopairs = Dict{String, String}(
+    "Zero Emissions & SSP1" => ("ssp1", "zero"),
+    "1.5 degC Target" => ("ssp1", "rcp26extra"),
+    "2 degC Target" => ("ssp1", "rcp26extra"),
+    "2.5 degC Target" => ("sspw", "rcpw"),
+    "NDCs" => ("sspw", "rcpw"),
+    "NDCs Partial" => ("sspw", "rcpw"),
+    "BAU" => ("sspw", "rcpw"),
+    "RCP2.6 & SSP1" => ("ssp1", "rcp26"),
+    "RCP4.5 & SSP2" => ("ssp2", "rcp45"),
+    "RCP8.5 & SSP5" => ("ssp5", "rcp85"))
+
+scenariopair = "NDCs"
