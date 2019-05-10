9c4a4f10409721eefee2700f9d3e83e2d8bac5fa
P-Wlich <paul.waidelich@gmx.de>
2019-05-10 13:30:00 +0100

    additional comment

--- src/getscc.jl
+++ src/getscc.jl
@@ -20,6 +20,7 @@ run(m1)
 if m1[:co2emissions, :e_globalCO2emissions][2] == m[:co2emissions, :e_globalCO2emissions][2]
     # pulse is 10^6 tCO2 and te_totaleffect is measured in million dollars --> no need for normalisation
     scc = (m1[:EquityWeighting, :te_totaleffect] - m[:EquityWeighting, :te_totaleffect]) / scc_pulse
+    # Note: disaggregation does not work as abatement costs are driven by baseline emissions and growth rate, not by levels at time t
     scc_imp = (m1[:EquityWeighting, :td_totaldiscountedimpacts] - m[:EquityWeighting, :td_totaldiscountedimpacts]) / scc_pulse
     scc_adt = (m1[:EquityWeighting, :tac_totaladaptationcosts] - m[:EquityWeighting, :tac_totaladaptationcosts]) / scc_pulse
     scc_abm = (m1[:EquityWeighting, :tpc_totalaggregatedcosts] - m[:EquityWeighting, :tpc_totalaggregatedcosts]) / scc_pulse
