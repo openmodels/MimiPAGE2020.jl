6debb8e3f8215d03c4a8d0a3fdef97e151caac2a
fhdezram <47187805+fhdezram@users.noreply.github.com>
2019-07-31 10:44:49 +0100

    Non-market Damages and Total Adaptation Costs updates
    I updated the Non-market Damages including mortality damages and other non-market impacts.
    I updated Total Adaptation Costs to include Mortality Adaptation Costs.
    I updated the equations coefficients.

--- src/components/NonMarketDamages.jl
+++ src/components/NonMarketDamages.jl
@@ -1,55 +1,132 @@
-
+using Mimi
 
 @defcomp NonMarketDamages begin
     region = Index()
 
     y_year = Parameter(index=[time], unit="year")
 
-    #parameters
-    rtl_realizedtemperature = Parameter(index=[time, region], unit="degreeC")
+    #Mortality parameters
+    rt_g_globaltemperature = Parameter(index=[time], unit="degreeC")
     rgdp_per_cap_MarketRemainGDP = Parameter(index=[time, region], unit = "\$/person")
     pop_population = Parameter(index=[time, region], unit="million person")
     VSL = Parameter(index=[region], unit="\$/")
-    rcons_per_cap_MarketRemainConsumption = Parameter(index=[time, region], unit = "\$/person")
-    rgdp_per_cap_MarketRemainGDP = Parameter(index=[time, region], unit = "\$/person")
-    save_savingsrate = Parameter(unit= "%", default=15.)
+
     b0m = Parameter(index=[region])
     b1m = Parameter(index=[region])
     b2m = Parameter(index=[region])
     b3m = Parameter(index=[region])
-    b0c = Parameter(index=[region])
-    b1c = Parameter(index=[region])
-    b2c = Parameter(index=[region])
-    b3c = Parameter(index=[region])
 
-     #parameters that are not required but otherwise the code does not run
+
+    # Other non-market parameters
+
+    rtl_realizedtemperature = Parameter(index=[time, region], unit="degreeC")
     impmax_maxtempriseforadaptpolicyNM = Parameter(index=[region], unit= "degreeC")
     atl_adjustedtolerableleveloftemprise = Parameter(index=[time,region], unit="degreeC")
     imp_actualreduction = Parameter(index=[time, region], unit= "%")
     isatg_impactfxnsaturation = Parameter(unit="unitless")
+    wincf_weightsfactor =Parameter(index=[region], unit="unitless")
+    w_NonImpactsatCalibrationTemp =Parameter(unit="%GDP", default=0.5333333333333333)
+    ipow_NonMarketIncomeFxnExponent =Parameter(unit="unitless", default=0.)
+    iben_NonMarketInitialBenefit=Parameter(unit="%GDP/degreeC", default=0.08333333333333333)
+    tcal_CalibrationTemp = Parameter(unit="degreeC", default=3.)
+    GDP_per_cap_focus_0_FocusRegionEU = Parameter(unit="\$/person", default=27934.244777382406)
+    pow_NonMarketExponent = Parameter(unit="", default=2.1666666666666665)
 
-    #variables
-    deaths = Variable(index=[time,region], unit="person")
+
+    # Mortality and other non-market parameters
+
+    fothernonmarketd = Parameter(unit = "unitless", default = 0.4)
+    rcons_per_cap_MarketRemainConsumption = Parameter(index=[time, region], unit = "\$/person")
+    rgdp_per_cap_MarketRemainGDP = Parameter(index=[time, region], unit = "\$/person")
+    save_savingsrate = Parameter(unit= "%", default=15.)
+
+    #Mortality variables
+    deaths = Variable(index=[time,region], unit="million person")
     mort_damages = Variable(index=[time, region], unit = "deaths/person")
-    mort_impacts = Variable(index=[time, region], unit = "\$/")
+    mort_impacts = Variable(index=[time, region], unit = "\$million")
     mort_impactspercap = Variable(index=[time, region], unit = "\$/person")
+
+    #Other non-market variables
+    i_regionalimpact = Variable(index=[time, region], unit="degreeC")
+    iref_ImpactatReferenceGDPperCap=Variable(index=[time, region], unit="%")
+    igdp_ImpactatActualGDPperCap=Variable(index=[time, region], unit="%")
+    isat_ImpactinclSaturationandAdaptation= Variable(index=[time,region], unit="\$")
+    isat_per_cap_ImpactperCapinclSaturationandAdaptation = Variable(index=[time,region], unit="\$/person")
+    other_non_market_impact= Variable(index=[time,region], unit="\$million")
+
+
+    # Mortality and other non-market variables
     rcons_per_cap_NonMarketRemainConsumption = Variable(index=[time, region], unit = "\$/person")
     rgdp_per_cap_NonMarketRemainGDP = Variable(index=[time, region], unit = "\$/person")
+    non_market_impact= Variable(index=[time,region], unit="\$")
+    non_market_impact_per_cap=Variable(index=[time, region], unit = "\$/person")
+
+
 
     function run_timestep(p, v, d, t)
 
         for r in d.region
 
-            v.mort_damages[t,r] = (p.b0m[r]+p.b0c[r])*p.rtl_realizedtemperature[t,r]+(p.b1m[r]+p.b1c[r])*(p.rtl_realizedtemperature[t,r])^2+(p.b2m[r]+p.b2c[r])*p.rtl_realizedtemperature[t,r]*log(p.rgdp_per_cap_MarketRemainGDP[t,r])+(p.b3m[r]+p.b3c[r])*((p.rtl_realizedtemperature[t,r])^2)*log(p.rgdp_per_cap_MarketRemainGDP[t,r])
+        # Mortality impacts
+
+     v.mort_damages[t,r] = (p.b0m[r])*p.rt_g_globaltemperature[t]+(p.b1m[r])*(p.rt_g_globaltemperature[t])^2+(p.b2m[r])*p.rt_g_globaltemperature[t]*p.y_year[t]+(p.b3m[r])*((p.rt_g_globaltemperature[t])^2)*p.y_year[t]
 
             v.deaths[t,r] = v.mort_damages[t,r]*p.pop_population[t,r]
 
             v.mort_impacts[t,r] = v.deaths[t,r]*p.VSL[r]
 
-            v.mort_impactspercap[t,r] = v.mort_impacts[t,r]/p.pop_population[t,r]
+            v.mort_impactspercap[t,r] = v.mort_impacts[t,r]/(p.pop_population[t,r])
 
-            v.rcons_per_cap_NonMarketRemainConsumption[t,r] = p.rcons_per_cap_MarketRemainConsumption[t,r] - v.mort_impactspercap[t,r]
+        # Other non-market impacts
+
+if p.rtl_realizedtemperature[t,r]-p.atl_adjustedtolerableleveloftemprise[t,r] < 0
+                v.i_regionalimpact[t,r] = 0
+            else
+                v.i_regionalimpact[t,r] = p.rtl_realizedtemperature[t,r]-p.atl_adjustedtolerableleveloftemprise[t,r]
+            end
+
+            v.iref_ImpactatReferenceGDPperCap[t,r]= p.wincf_weightsfactor[r]*
+                ((p.w_NonImpactsatCalibrationTemp + p.iben_NonMarketInitialBenefit *p.tcal_CalibrationTemp)*
+                    (v.i_regionalimpact[t,r]/p.tcal_CalibrationTemp)^p.pow_NonMarketExponent - v.i_regionalimpact[t,r] * p.iben_NonMarketInitialBenefit)
+
+            v.igdp_ImpactatActualGDPperCap[t,r]= v.iref_ImpactatReferenceGDPperCap[t,r]*
+                (p.rgdp_per_cap_MarketRemainGDP[t,r]/p.GDP_per_cap_focus_0_FocusRegionEU)^p.ipow_NonMarketIncomeFxnExponent
+
+                if v.igdp_ImpactatActualGDPperCap[t,r] < p.isatg_impactfxnsaturation
+                    v.isat_ImpactinclSaturationandAdaptation[t,r] = v.igdp_ImpactatActualGDPperCap[t,r]
+                else
+                    v.isat_ImpactinclSaturationandAdaptation[t,r] = p.isatg_impactfxnsaturation+
+                        ((100-p.save_savingsrate)-p.isatg_impactfxnsaturation)*
+                        ((v.igdp_ImpactatActualGDPperCap[t,r]-p.isatg_impactfxnsaturation)/
+                        (((100-p.save_savingsrate)-p.isatg_impactfxnsaturation)+
+                        (v.igdp_ImpactatActualGDPperCap[t,r]-
+                        p.isatg_impactfxnsaturation)))
+                    end
+
+                if v.i_regionalimpact[t,r] < p.impmax_maxtempriseforadaptpolicyNM[r]
+                    v.isat_ImpactinclSaturationandAdaptation[t,r]=v.isat_ImpactinclSaturationandAdaptation[t,r]*(1-p.imp_actualreduction[t,r]/100)
+                else
+                    v.isat_ImpactinclSaturationandAdaptation[t,r] = v.isat_ImpactinclSaturationandAdaptation[t,r] *
+                        (1-(p.imp_actualreduction[t,r]/100)* p.impmax_maxtempriseforadaptpolicyNM[r] /
+                        v.i_regionalimpact[t,r])
+                end
+
+            v.isat_per_cap_ImpactperCapinclSaturationandAdaptation[t,r] = (v.isat_ImpactinclSaturationandAdaptation[t,r]/100)*p.rgdp_per_cap_MarketRemainGDP[t,r]
+            v.other_non_market_impact[t,r]=p.fothernonmarketd*v.isat_per_cap_ImpactperCapinclSaturationandAdaptation[t,r]*(p.pop_population[t,r])
+
+         # Total non-market impacts
+
+            v.non_market_impact[t,r]=v.mort_impacts[t,r]+v.other_non_market_impact[t,r]
+
+            v.non_market_impact_per_cap[t,r]=v.non_market_impact[t,r]/(p.pop_population[t,r])
+
+
+
+            v.rcons_per_cap_NonMarketRemainConsumption[t,r] = p.rcons_per_cap_MarketRemainConsumption[t,r]-v.non_market_impact_per_cap[t,r]
             v.rgdp_per_cap_NonMarketRemainGDP[t,r] = v.rcons_per_cap_NonMarketRemainConsumption[t,r]/(1-p.save_savingsrate/100)
+
+
+
 
         end
     end
--- src/components/TotalAdaptationCosts.jl
+++ src/components/TotalAdaptationCosts.jl
@@ -1,20 +1,56 @@
 
+using Mimi
 
 @defcomp TotalAdaptationCosts begin
     region = Index()
+    y_year = Parameter(index=[time], unit="year")
+
+    #Mortality adaptation costs parameters
+    fothernonmarketc = Parameter(unit = "unitless", default = 0.4)
+    rt_g_globaltemperature = Parameter(index=[time], unit="degreeC")
+    pop_population = Parameter(index=[time, region], unit="million person")
+    VSL = Parameter(index=[region], unit="\$/")
+    b0c = Parameter(index=[region])
+    b1c = Parameter(index=[region])
+    b2c = Parameter(index=[region])
+    b3c = Parameter(index=[region])
+
+
+    #Mortality adaptation costs variables
+    deaths = Variable(index=[time,region], unit="million person")
+    mort_adaptation_costs_damages = Variable(index=[time, region], unit = "deaths/person")
+    mort_adaptation_costs = Variable(index=[time, region], unit = "\$/")
+    mort_adaptationcosts_percap = Variable(index=[time, region], unit = "\$/person")
+
+
     #Total Adaptation Costs
     pop_population = Parameter(index=[time, region], unit= "million person")
     ac_adaptationcosts_economic = Parameter(index=[time, region], unit="\$million")
     ac_adaptationcosts_noneconomic = Parameter(index=[time, region], unit="\$million")
     ac_adaptationcosts_sealevelrise = Parameter(index=[time, region], unit="\$million")
 
+    non_economic_adaptation_costs=Variable(index=[time, region], unit="\$")
     act_adaptationcosts_total = Variable(index=[time, region], unit="\$million")
     act_percap_adaptationcosts = Variable(index=[time, region], unit="\$/person")
 
     function run_timestep(p, v, d, t)
 
         for r in d.region
-            v.act_adaptationcosts_total[t,r] = p.ac_adaptationcosts_economic[t,r] + p.ac_adaptationcosts_sealevelrise[t,r] + p.ac_adaptationcosts_noneconomic[t,r]
+
+             # Mortality adaptation costs
+
+     v.mort_adaptation_costs_damages[t,r] =  (p.b0c[r])*p.rt_g_globaltemperature[t]+(p.b1c[r])*(p.rt_g_globaltemperature[t])^2+(p.b2c[r])*p.rt_g_globaltemperature[t]*p.y_year[t]+(p.b3c[r])*((p.rt_g_globaltemperature[t])^2)*p.y_year[t]
+
+
+            v.deaths[t,r] = v.mort_adaptation_costs_damages[t,r]*p.pop_population[t,r]
+
+            v.mort_adaptation_costs[t,r] = v.deaths[t,r]*p.VSL[r]
+
+            v.mort_adaptationcosts_percap[t,r] = v.mort_adaptation_costs[t,r]/(p.pop_population[t,r])
+
+            v.non_economic_adaptation_costs[t,r]=v.mort_adaptation_costs[t,r]+p.ac_adaptationcosts_noneconomic[t,r]*p.fothernonmarketc
+
+            v.act_adaptationcosts_total[t,r] = p.ac_adaptationcosts_economic[t,r] + p.ac_adaptationcosts_sealevelrise[t,r]+ v.non_economic_adaptation_costs[t,r]
             v.act_percap_adaptationcosts[t,r] = v.act_adaptationcosts_total[t,r]/p.pop_population[t,r]
         end
     end
