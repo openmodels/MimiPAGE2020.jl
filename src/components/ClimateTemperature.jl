a01f9978e90d9695d8498acd631012c514f91a4c
Jarmo Kikstra <jskikstra@gmail.com>
2019-11-11 23:16:42 +0100

    add annual resolution to ClimateTemperature module

--- src/components/ClimateTemperature.jl
+++ src/components/ClimateTemperature.jl
@@ -1,10 +1,49 @@
 @defcomp ClimateTemperature begin
+
+    function calc_temp(p, v, d, tt, annual_year)
+        # for every year, do the same calculations, but then with the new annual_year variables
+        yr = annual_year - 2015 + 1 # + 1 because of 1-based indexing in Julia
+
+        # linear interpolation between the years of analysis, forward-stepping, up to gettime(tt)
+        ## interpolation here is also linear for 2015-2020
+        if is_first(tt)
+            frac = annual_year - 2015
+            fraction_timestep = frac/((gettime(tt))-2015)
+
+            v.pt_g_preliminarygmst_ann[yr] = (v.pt_g_preliminarygmst[tt])*(fraction_timestep) + p.rt_g0_baseglobaltemp*(1-fraction_timestep)  # difference_to_next_tt * ratio_to_there
+        else
+            frac = annual_year - gettime(tt-1)
+            fraction_timestep = frac/((gettime(tt))-(gettime(tt-1))) # check if +1 might also need to feature here.
+
+            v.pt_g_preliminarygmst_ann[yr] = (v.pt_g_preliminarygmst[tt] )*(fraction_timestep) + v.pt_g_preliminarygmst[tt-1]*(1-fraction_timestep) # difference_to_next_tt * ratio_to_there
+        end
+        # Without surface albedo, just equal
+        v.rt_g_globaltemperature_ann[yr] = v.pt_g_preliminarygmst_ann[yr] # this feeds into Discontinuity.jl, gets variability through pt_g_preliminarygmst -> pt_g_preliminarygmst_ann
+
+        # Adding adjustment, from Hope (2009)
+        for r in d.region
+            v.rtl_realizedtemperature_ann[yr, r] = v.rt_g_globaltemperature_ann[yr] * p.ampf_amplification[r]
+        end
+
+        v.rt_g_globaltemperature_ann[yr] = v.pt_g_preliminarygmst_ann[yr]
+
+        # Land average temperature
+        v.rtl_g_landtemperature_ann[yr] = sum(v.rtl_realizedtemperature_ann[yr, :]' .* p.area') / sum(p.area)
+
+        # Ocean average temperature
+        v.rto_g_oceantemperature_ann[yr] = (p.area_e_eartharea * v.rt_g_globaltemperature_ann[yr] - sum(p.area) * v.rtl_g_landtemperature_ann[yr]) / (p.area_e_eartharea - sum(p.area))
+
+    end
+
+
     region = Index()
+    year = Index()
 
     # Basic parameters
     area = Parameter(index=[region], unit="km2")
     y_year_0 = Parameter(unit="year")
     y_year = Parameter(index=[time], unit="year")
+    y_year_ann = Parameter(index=[year], unit="year")
     area_e_eartharea = Parameter(unit="km2", default=5.1e8)
     use_seaice::Bool = Parameter()
 
@@ -48,13 +87,17 @@
 
     # Global outputs
     rt_g_globaltemperature = Variable(index=[time], unit="degreeC")
+    rt_g_globaltemperature_ann = Variable(index=[year], unit="degreeC")
     rto_g_oceantemperature = Variable(index=[time], unit="degreeC")
+    rto_g_oceantemperature_ann = Variable(index=[year], unit="degreeC")
     rtl_g_landtemperature = Variable(index=[time], unit="degreeC")
+    rtl_g_landtemperature_ann = Variable(index=[year], unit="degreeC")
 
     # Regional outputs
     ampf_amplification = Parameter(index=[region])
 
     rtl_realizedtemperature = Variable(index=[time, region], unit="degreeC")
+    rtl_realizedtemperature_ann = Variable(index=[year, region], unit="degreeC")
 
     function init(p, v, d)
         for rr in d.region
@@ -138,6 +181,17 @@
 
         # Ocean average temperature
         v.rto_g_oceantemperature[tt] = (p.area_e_eartharea * v.rt_g_globaltemperature[tt] - sum(p.area) * v.rtl_g_landtemperature[tt]) / (p.area_e_eartharea - sum(p.area))
+
+        # annual interpolation of the timestep-calculated variables.
+        if is_first(tt)
+            for annual_year = 2015:gettime(tt) # because p["y_year_0"] does not exist (globally)
+                calc_temp(p, v, d, tt, annual_year) # NEW -- introduce function for calulating temperature
+            end
+        else
+            for annual_year = (gettime(tt-1)+1):gettime(tt)
+                calc_temp(p, v, d, tt, annual_year) # NEW -- use  function for calulating temperature
+            end
+        end
     end
 end
 
