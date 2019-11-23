9b4626144eefb61399dcb2e7ab67ff3c0d5ea2fd
jkikstra <46967503+jkikstra@users.noreply.github.com>
2019-11-23 14:49:38 +0100

    add interannual variability to PAGE

---
+++ src/run_pagevar.jl
@@ -0,0 +1,39 @@
+using Mimi
+
+function set_globalbools()
+    global use_variability = true
+    global use_annual = true
+
+
+    global use_linear = false
+    global use_logburke = false
+    global use_logpopulation = false
+    global use_logwherepossible = true
+end
+
+# set global values for technical configuration options
+set_globalbools()
+
+#run main_model file
+include("main_model.jl")
+
+# get/define model, with default settings (i.e. NDCs scenario, permafrost, no sea-ice, use_page09damages)
+# m = getpage()
+# or alternatively, with different configurations:
+# m = getpage("2 degC Target", true, true)
+m = getpage("NDCs", true, false)
+# run model
+run(m)
+
+# get the social cost of carbon
+scc = compute_scc(m, year=2020)
+println(scc)
+
+# open up Explorer UI, for visual exploration of the variables
+# explore(m)
+
+
+# get the social cost of carbon for the Monte Carlo simulations, for selected quantiles.
+# sccs = compute_scc_mcs(m, 5000, year=2020)
+# sccobs = [quantile(sccs, [.05, .25, .5, .75, .95]); mean(sccs)]
+# println(sccobs)
