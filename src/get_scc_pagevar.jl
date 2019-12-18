9dec85a0511ba61219aa9264b52efe1ceed1f8ca
jkikstra <46967503+jkikstra@users.noreply.github.com>
2019-12-18 17:12:27 +0100

    create dedicated scripts for outputting MC scc values

---
+++ src/get_scc_pagevar.jl
@@ -0,0 +1,43 @@
+using Mimi
+using CSV
+using DataFrames
+
+function set_globalbools()
+    global use_variability = true
+    global use_annual = true
+
+    # set random seed to have similar variability development in the base and the marginal model.
+    # set variability seed.
+    if use_variability
+        global varseed = rand(1:1000000000000)
+    end
+
+    global use_linear = false
+    global use_logburke = false
+    global use_logpopulation = false
+    global use_logwherepossible = true
+end
+
+# set global values for technical configuration options
+set_globalbools()
+
+# get main_model file
+include("main_model.jl")
+include("mcs.jl")
+
+for scenario in ["1.5 degC Target", "2 degC Target", "RCP2.6 & SSP1", "RCP4.5 & SSP2", "RCP8.5 & SSP5"]
+    model = "PAGE-VAR"
+    # define model, default settings: getpage(NDCs scenario, permafrost, no sea-ice, no page09damages)
+    m = getpage(scenario, true, true)
+    # run model
+    run(m)
+
+    # get the social cost of carbon for the Monte Carlo simulations, for selected quantiles.
+    samplesize = 50000
+    sccs = compute_scc_mcs(m, samplesize, year=2020)
+    # store results in DataFrame
+    df = DataFrame(Any[fill(model, samplesize), fill(scenario, samplesize), sccs], [:ModelName, :ScenarioName, :SCC])
+    # write out to csv
+    DIR = joinpath(@__DIR__, "..", "output")
+    CSV.write(joinpath(DIR, string(model, "_", scenario, "_", "scc.csv")), df)
+end
