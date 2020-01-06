7d18321a3095bc168f00bf2fed3acc2f77a423eb
jkikstra <46967503+jkikstra@users.noreply.github.com>
2020-01-06 20:39:05 +0100

    create separate files for running PAGE with annual timestep

---
+++ src/components/Discontinuity_annual.jl
@@ -0,0 +1,182 @@
+function calc_discontinuity(p, v, d, t, annual_year, r)
+    # setting the year for entry in lists.
+    yr = annual_year - 2015 + 1 # + 1 because of 1-based indexing in Julia
+
+    # global variables only have to be done once. [r goes from 1 to 8]
+    if isequal(r, 1)
+
+        v.idis_lossfromdisc_ann[yr] = max(0, p.rt_g_globaltemperature_ann[yr] - p.tdis_tolerabilitydisc) # global temperature minus 1.5 (default)
+
+        if is_first(t)
+            if v.idis_lossfromdisc_ann[yr]*(p.pdis_probability/100) > p.rand_discontinuity # (temp - 1.5)*0.2 >? .5 (default)
+                v.occurdis_occurrencedummy_ann[yr] = 1
+            else
+                v.occurdis_occurrencedummy_ann[yr] = 0
+            end
+            v.expfdis_discdecay_ann[yr]=exp(-(p.y_year[t] - p.y_year_0)/p.distau_discontinuityexponent) # left same as timestep version
+        else
+            if v.idis_lossfromdisc_ann[yr]*(p.pdis_probability/100) > p.rand_discontinuity
+                v.occurdis_occurrencedummy_ann[yr] = 1
+            elseif v.occurdis_occurrencedummy_ann[yr-1] == 1
+                v.occurdis_occurrencedummy_ann[yr] = 1
+            else
+                v.occurdis_occurrencedummy_ann[yr] = 0
+            end
+            v.expfdis_discdecay_ann[yr]=exp(-(p.y_year[t] - p.y_year[t-1])/p.distau_discontinuityexponent) # left same as timestep version
+        end
+    end
+
+    v.igdpeqdis_eqdiscimpact_ann[yr,r] = v.irefeqdis_eqdiscimpact[r] * (p.rgdp_per_cap_NonMarketRemainGDP_ann[yr,r]/p.GDP_per_cap_focus_0_FocusRegionEU)^p.ipow_incomeexponent
+
+    if is_first(t)
+        v.igdp_realizeddiscimpact_ann[yr,r]=v.occurdis_occurrencedummy_ann[yr]*(1-v.expfdis_discdecay_ann[yr])*v.igdpeqdis_eqdiscimpact_ann[yr,r]
+    else
+        v.igdp_realizeddiscimpact_ann[yr,r]=v.igdp_realizeddiscimpact_ann[yr-1,r]+v.occurdis_occurrencedummy_ann[yr]*(1-v.expfdis_discdecay_ann[yr])*(v.igdpeqdis_eqdiscimpact_ann[yr,r]-v.igdp_realizeddiscimpact_ann[yr-1,r])
+    end
+
+    if v.igdp_realizeddiscimpact_ann[yr,r] < p.isatg_saturationmodification    #### analyse this in dissertation?
+        v.isat_satdiscimpact_ann[yr,r] = v.igdp_realizeddiscimpact_ann[yr,r]
+    else
+        v.isat_satdiscimpact_ann[yr,r] = p.isatg_saturationmodification + (100-p.isatg_saturationmodification)*((v.igdp_realizeddiscimpact_ann[yr,r]-p.isatg_saturationmodification)/((100-p.isatg_saturationmodification)+(v.igdp_realizeddiscimpact_ann[yr,r] - p.isatg_saturationmodification)))
+    end
+
+    v.isat_per_cap_DiscImpactperCapinclSaturation_ann[yr,r] = (v.isat_satdiscimpact_ann[yr,r]/100)*p.rgdp_per_cap_NonMarketRemainGDP_ann[yr,r]
+    v.rcons_per_cap_DiscRemainConsumption_ann[yr,r] = p.rcons_per_cap_NonMarketRemainConsumption_ann[yr,r] - v.isat_per_cap_DiscImpactperCapinclSaturation_ann[yr,r]
+
+end
+
+
+@defcomp Discontinuity begin
+
+  region = Index()
+  year = Index()
+  y_year=Parameter(index=[time], unit="year")
+  y_year_0 = Parameter(unit="year")
+  y_year_ann = Parameter(index=[year], unit="year")
+  yagg_periodspan = Parameter(index=[time], unit="year") # for doing in-component aggregation
+
+  rand_discontinuity = Parameter(unit="unitless", default=.5)
+
+  irefeqdis_eqdiscimpact=Variable(index=[region], unit="%")
+  wincf_weightsfactor_sea = Parameter(index=[region], unit="")
+  wdis_gdplostdisc=Parameter(unit="%", default=3.)
+
+  igdpeqdis_eqdiscimpact=Variable(index=[time,region], unit="%")
+  igdpeqdis_eqdiscimpact_ann=Variable(index=[year,region], unit="%")
+  rgdp_per_cap_NonMarketRemainGDP=Parameter(index=[time,region], unit="\$/person")
+  rgdp_per_cap_NonMarketRemainGDP_ann=Parameter(index=[year,region], unit="\$/person")
+  GDP_per_cap_focus_0_FocusRegionEU = Parameter(unit="\$/person", default=34298.93698672955)
+  ipow_incomeexponent=Parameter(unit="unitless", default=-0.13333333333333333)
+
+  igdp_realizeddiscimpact=Variable(index=[time,region], unit="%")
+  igdp_realizeddiscimpact_ann = Variable(index=[year,region], unit="%")
+  occurdis_occurrencedummy=Variable(index=[time], unit="unitless")
+  occurdis_occurrencedummy_sum = Variable(unit="unitless") # for analysis
+  occurdis_occurrencedummy_ann = Variable(index=[year], unit="unitless")
+  occurdis_occurrencedummy_ann_sum = Variable(unit="unitless") # for analysis
+  expfdis_discdecay=Variable(index=[time], unit="unitless")
+  expfdis_discdecay_ann = Variable(index=[year], unit="unitless")
+
+
+  distau_discontinuityexponent=Parameter(unit="unitless", default=20.)
+
+  idis_lossfromdisc=Variable(index=[time], unit="degreeC")
+  idis_lossfromdisc_ann = Variable(index=[year], unit="degreeC")
+  tdis_tolerabilitydisc=Parameter(unit="degreeC", default=1.5)
+  rt_g_globaltemperature = Parameter(index=[time], unit="degreeC")
+  rt_g_globaltemperature_ann = Parameter(index=[year], unit="degreeC")
+  pdis_probability=Parameter(unit="%/degreeC", default=20.)
+
+  isatg_saturationmodification = Parameter(unit="unitless")
+  isat_satdiscimpact=Variable(index=[time,region], unit="%")
+  isat_satdiscimpact_ann = Variable(index=[year,region], unit="%")
+
+  isat_per_cap_DiscImpactperCapinclSaturation=Variable(index=[time,region], unit="\$/person")
+  isat_per_cap_DiscImpactperCapinclSaturation_sum=Variable(unit="\$/person") # for analysis
+  isat_per_cap_DiscImpactperCapinclSaturation_ann = Variable(index=[year,region], unit="\$/person")
+  isat_per_cap_DiscImpactperCapinclSaturation_ann_sum=Variable(unit="\$/person") # for analysis
+  rcons_per_cap_DiscRemainConsumption=Variable(index=[time, region], unit = "\$/person")
+  rcons_per_cap_DiscRemainConsumption_sum =  Variable(unit = "\$/person") # for analysis
+  rcons_per_cap_DiscRemainConsumption_ann = Variable(index=[year, region], unit = "\$/person")
+  rcons_per_cap_DiscRemainConsumption_ann_sum = Variable(unit = "\$/person") # for analysis
+  rcons_per_cap_NonMarketRemainConsumption = Parameter(index=[time, region], unit = "\$/person")
+  rcons_per_cap_NonMarketRemainConsumption_ann = Parameter(index=[year, region], unit = "\$/person")
+
+
+    function run_timestep(p, v, d, t)
+
+        v.idis_lossfromdisc[t] = max(0, p.rt_g_globaltemperature[t] - p.tdis_tolerabilitydisc)
+
+        if is_first(t)
+            if v.idis_lossfromdisc[t]*(p.pdis_probability/100) > p.rand_discontinuity
+                v.occurdis_occurrencedummy[t] = 1
+            else
+                v.occurdis_occurrencedummy[t] = 0
+            end
+            v.expfdis_discdecay[t]=exp(-(p.y_year[t] - p.y_year_0)/p.distau_discontinuityexponent)
+        else
+            if v.idis_lossfromdisc[t]*(p.pdis_probability/100) > p.rand_discontinuity
+                v.occurdis_occurrencedummy[t] = 1
+            elseif v.occurdis_occurrencedummy[t-1] == 1
+                v.occurdis_occurrencedummy[t] = 1
+            else
+                v.occurdis_occurrencedummy[t] = 0
+            end
+            v.expfdis_discdecay[t]=exp(-(p.y_year[t] - p.y_year[t-1])/p.distau_discontinuityexponent)
+        end
+
+        for r in d.region
+            v.irefeqdis_eqdiscimpact[r] = p.wincf_weightsfactor_sea[r]*p.wdis_gdplostdisc
+
+            v.igdpeqdis_eqdiscimpact[t,r] = v.irefeqdis_eqdiscimpact[r] * (p.rgdp_per_cap_NonMarketRemainGDP[t,r]/p.GDP_per_cap_focus_0_FocusRegionEU)^p.ipow_incomeexponent
+
+            if is_first(t)
+                v.igdp_realizeddiscimpact[t,r]=v.occurdis_occurrencedummy[t]*(1-v.expfdis_discdecay[t])*v.igdpeqdis_eqdiscimpact[t,r]
+            else
+                v.igdp_realizeddiscimpact[t,r]=v.igdp_realizeddiscimpact[t-1,r]+v.occurdis_occurrencedummy[t]*(1-v.expfdis_discdecay[t])*(v.igdpeqdis_eqdiscimpact[t,r]-v.igdp_realizeddiscimpact[t-1,r])
+            end
+
+            if v.igdp_realizeddiscimpact[t,r] < p.isatg_saturationmodification
+                v.isat_satdiscimpact[t,r] = v.igdp_realizeddiscimpact[t,r]
+            else
+                v.isat_satdiscimpact[t,r] = p.isatg_saturationmodification + (100-p.isatg_saturationmodification)*((v.igdp_realizeddiscimpact[t,r]-p.isatg_saturationmodification)/((100-p.isatg_saturationmodification)+(v.igdp_realizeddiscimpact[t,r] - p.isatg_saturationmodification)))
+            end
+            v.isat_per_cap_DiscImpactperCapinclSaturation[t,r] = (v.isat_satdiscimpact[t,r]/100)*p.rgdp_per_cap_NonMarketRemainGDP[t,r]
+            v.rcons_per_cap_DiscRemainConsumption[t,r] = p.rcons_per_cap_NonMarketRemainConsumption[t,r] - v.isat_per_cap_DiscImpactperCapinclSaturation[t,r]
+
+            # calculate  for this specific year
+            if is_first(t)
+                # v.rcons_per_cap_DiscRemainConsumption_ann_sum = 0
+                for annual_year = 2015:(gettime(t))
+                    calc_discontinuity(p, v, d, t, annual_year, r)
+                end
+                if isequal(r,8)
+                    v.occurdis_occurrencedummy_sum = v.occurdis_occurrencedummy[t]*p.yagg_periodspan[t]
+                    v.rcons_per_cap_DiscRemainConsumption_sum = sum(v.rcons_per_cap_DiscRemainConsumption[t,:])*p.yagg_periodspan[t]
+                    v.isat_per_cap_DiscImpactperCapinclSaturation_sum = sum(v.isat_per_cap_DiscImpactperCapinclSaturation[t,:])*p.yagg_periodspan[t]
+                end
+            else
+                for annual_year = (gettime(t-1)+1):(gettime(t))
+                     calc_discontinuity(p, v, d, t, annual_year, r)
+
+
+                     if isequal(annual_year, 2300)
+                         if isequal(r,8)
+                             v.occurdis_occurrencedummy_ann_sum = sum(v.occurdis_occurrencedummy_ann[:])
+                             v.rcons_per_cap_DiscRemainConsumption_ann_sum =  sum(v.rcons_per_cap_DiscRemainConsumption_ann[:,:])
+                             v.isat_per_cap_DiscImpactperCapinclSaturation_ann_sum = sum(v.isat_per_cap_DiscImpactperCapinclSaturation_ann[:,:])
+                         end
+                     end
+                end
+                if isequal(r,8)
+                    # for analysis
+                    v.occurdis_occurrencedummy_sum = v.occurdis_occurrencedummy_sum + v.occurdis_occurrencedummy[t]*p.yagg_periodspan[t]
+                    v.rcons_per_cap_DiscRemainConsumption_sum = v.rcons_per_cap_DiscRemainConsumption_sum + sum(v.rcons_per_cap_DiscRemainConsumption[t,:])*p.yagg_periodspan[t]
+                    v.isat_per_cap_DiscImpactperCapinclSaturation_sum = v.isat_per_cap_DiscImpactperCapinclSaturation_sum + sum(v.isat_per_cap_DiscImpactperCapinclSaturation[t,:])*p.yagg_periodspan[t]
+                end
+
+            end
+
+        end
+    end
+end
---
+++ src/compute_scc_annual.jl
@@ -0,0 +1,171 @@
+using Mimi
+using Random
+
+page_years = [2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200, 2250, 2300]
+page_year_0 = 2015
+
+function getpageindexfromyear(year)
+    i = findfirst(isequal(year), page_years)
+    if i == 0
+        error("Invalid PAGE year: $year.")
+    end
+    return i
+end
+
+function getperiodlength(year)      # same calculations made for yagg_periodspan in the model
+    i = getpageindexfromyear(year)
+
+    if year==page_years[1]
+        start_year = page_year_0
+    else
+        start_year = page_years[i - 1]
+    end
+
+    if year == page_years[end]
+        last_year = page_years[end]
+    else
+        last_year = page_years[i + 1]
+    end
+
+    return (last_year - start_year) / 2
+end
+
+@defcomp PAGE_marginal_emissions begin
+    er_CO2emissionsgrowth = Variable(index=[time,region], unit = "%")
+    marginal_emissions_growth = Parameter(index=[time,region], unit = "%", default = zeros(10,8))
+    function run_timestep(p, v, d, t)
+        if is_first(t)
+            v.er_CO2emissionsgrowth[:, :] = p.marginal_emissions_growth[:, :]
+        end
+    end
+end
+
+"""
+compute_scc(m::Model = get_model(); year::Union{Int, Nothing} = nothing, eta::Union{Float64, Nothing} = nothing, prtp::Union{Float64, Nothing} = nothing)
+
+Computes the social cost of CO2 for an emissions pulse in `year` for the provided Mimi-PAGE model.
+If no model is provided, the default model from main_model.get_model() is used.
+Discounting scheme can be specified by the `eta` and `prtp` parameters, which will update the values of emuc_utilitiyconvexity and ptp_timepreference in the model.
+If no values are provided, the discount factors will be computed using the default PAGE values of emuc_utilitiyconvexity=1.1666666667 and ptp_timepreference=1.0333333333.
+"""
+function compute_scc(m::Model = get_model(); year::Union{Int, Nothing} = nothing, eta::Union{Float64, Nothing} = nothing, prtp::Union{Float64, Nothing} = nothing, pulse_size = 75000.)
+    year === nothing ? error("Must specify an emission year. Try `compute_scc(m, year=2020)`.") : nothing
+    !(year in page_years) ? error("Cannot compute the scc for year $year, year must be within the model's time index $page_years.") : nothing
+
+    eta == nothing ? nothing : update_param!(m, :emuc_utilityconvexity, eta)
+    prtp == nothing ? nothing : update_param!(m, :ptp_timepreference, prtp * 100.)
+
+    mm = get_marginal_model(m, year=year, pulse_size=pulse_size)   # Returns a marginal model that has already been run
+    if use_annual
+        scc = mm[:EquityWeighting, :td_totaldiscountedimpacts_ann]
+    else
+        scc = mm[:EquityWeighting, :td_totaldiscountedimpacts]
+    end
+
+    return scc
+end
+
+"""
+compute_scc_mm(m::Model = get_model(); year::Union{Int, Nothing} = nothing, eta::Union{Float64, Nothing} = nothing, prtp::Union{Float64, Nothing} = nothing)
+
+Returns a NamedTuple (scc=scc, mm=mm) of the social cost of carbon and the MarginalModel used to compute it.
+Computes the social cost of CO2 for an emissions pulse in `year` for the provided Mimi-PAGE model.
+If no model is provided, the default model from main_model.get_model() is used.
+Discounting scheme can be specified by the `eta` and `prtp` parameters, which will update the values of emuc_utilitiyconvexity and ptp_timepreference in the model.
+If no values are provided, the discount factors will be computed using the default PAGE values of emuc_utilitiyconvexity=1.1666666667 and ptp_timepreference=1.0333333333.
+"""
+function compute_scc_mm(m::Model = get_model(); year::Union{Int, Nothing} = nothing, eta::Union{Float64, Nothing} = nothing, prtp::Union{Float64, Nothing} = nothing, pulse_size = 75000.)
+    year === nothing ? error("Must specify an emission year. Try `compute_scc(m, year=2020)`.") : nothing
+    !(year in page_years) ? error("Cannot compute the scc for year $year, year must be within the model's time index $page_years.") : nothing
+
+    eta == nothing ? nothing : update_param!(m, :emuc_utilityconvexity, eta)
+    prtp == nothing ? nothing : update_param!(m, :ptp_timepreference, prtp * 100.)
+
+    mm = get_marginal_model(m, year=year, pulse_size=pulse_size)   # Returns a marginal model that has already been run
+
+    if use_annual
+        scc = mm[:EquityWeighting, :td_totaldiscountedimpacts_ann]
+        scc_disaggregated = mm[:EquityWeighting, :addt_equityweightedimpact_discountedaggregated_ann]
+    else
+        scc = mm[:EquityWeighting, :td_totaldiscountedimpacts]
+        scc_disaggregated = mm[:EquityWeighting, :addt_equityweightedimpact_discountedaggregated]
+    end
+
+    return (scc = scc, scc_disaggregated = scc_disaggregated, mm = mm)
+end
+
+"""
+get_marginal_model(m::Model = get_model(); year::Union{Int, Nothing} = nothing)
+Returns a Mimi MarginalModel where the provided m is the base model, and the marginal model has additional emissions of CO2 in year `year`.
+If no Model m is provided, the default model from main_model.get_model() is used as the base model.
+Note that the returned MarginalModel has already been run.
+"""
+function get_marginal_model(m::Model = get_model(); year::Union{Int, Nothing} = nothing, pulse_size = 75000.)#, varseed::Union{Int, Nothing} = nothing)
+    year === nothing ? error("Must specify an emission year. Try `get_marginal_model(m, year=2020)`.") : nothing
+    !(year in page_years) ? error("Cannot add marginal emissions in $year, year must be within the model's time index $page_years.") : nothing
+
+    mm = create_marginal_model(m, pulse_size)
+
+    add_comp!(mm.marginal, PAGE_marginal_emissions, :marginal_emissions; before = :co2emissions)
+    connect_param!(mm.marginal, :co2emissions=>:er_CO2emissionsgrowth, :marginal_emissions=>:er_CO2emissionsgrowth)
+    connect_param!(mm.marginal, :AbatementCostsCO2=>:er_emissionsgrowth, :marginal_emissions=>:er_CO2emissionsgrowth)
+
+    i = getpageindexfromyear(year)
+
+    # set random seed to have similar variability development in the base and the marginal model.
+    if use_variability
+        Random.seed!(varseed);
+    end
+    # run base model
+    run(mm.base)
+    base_glob0_emissions = mm.base[:CO2Cycle, :e0_globalCO2emissions]
+    er_co2_a = mm.base[:co2emissions, :er_CO2emissionsgrowth][i, :]
+    e_co2_g = mm.base[:co2emissions, :e_globalCO2emissions]
+
+    # Calculate pulse
+    ER_SCC = 100 * -1 * pulse_size / (base_glob0_emissions * getperiodlength(year))
+    pulse = er_co2_a - ER_SCC * (er_co2_a/100) * (base_glob0_emissions / e_co2_g[i])
+    marginal_emissions_growth = copy(mm.base[:co2emissions, :er_CO2emissionsgrowth])
+    marginal_emissions_growth[i, :] = pulse
+
+    # Marginal emissions model
+    update_param!(mm.marginal, :marginal_emissions_growth, marginal_emissions_growth)
+
+    # set random seed to have similar variability development in the base and the marginal model.
+    if use_variability
+        Random.seed!(varseed);
+    end
+    # run marginal model
+    run(mm.marginal)
+
+    return mm
+end
+
+function compute_scc_mcs(m::Model, samplesize::Int; year::Union{Int, Nothing} = nothing, eta::Union{Float64, Nothing} = nothing, prtp::Union{Float64, Nothing} = nothing, pulse_size = 75000.)#, varseed::Union{Int, Nothing} = nothing)
+    # Setup of location of final results
+    scc_results = zeros(samplesize)
+
+    function mc_scc_calculation(sim_inst::SimulationInstance, trialnum::Int, ntimesteps::Int, ignore::Nothing)
+        marginal = sim_inst.models[1]
+        if use_annual
+            marg_damages = marginal[:EquityWeighting, :td_totaldiscountedimpacts_ann]
+        else
+            marg_damages = marginal[:EquityWeighting, :td_totaldiscountedimpacts]
+        end
+        scc_results[trialnum] = marg_damages
+    end
+
+    # get simulation
+    mcs = getsim()
+
+    # Setup models
+    eta == nothing ? nothing : update_param!(m, :emuc_utilityconvexity, eta)
+    prtp == nothing ? nothing : update_param!(m, :ptp_timepreference, prtp * 100.)
+
+    mm = get_marginal_model(m, year=year, pulse_size=pulse_size)#, varseed=varseed)   # Returns a marginal model that has already been run
+
+    # Run
+    res = run(mcs, mm, samplesize; post_trial_func=mc_scc_calculation)
+
+    return scc_results
+end
