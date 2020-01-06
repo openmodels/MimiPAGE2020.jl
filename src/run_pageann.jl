cd042eb76c83bdf67994b3b081b6e094fc14aa89
jkikstra <46967503+jkikstra@users.noreply.github.com>
2020-01-06 20:42:28 +0100

    update runfiles (scenarios, delete obsolete SCC code)

--- src/run_pageann.jl
+++ src/run_pageann.jl
@@ -1,5 +1,6 @@
 using Mimi
 
+# specify model settings
 function set_globalbools()
     global use_variability = false
     global use_annual = true
@@ -23,37 +24,17 @@ set_globalbools()
 include("main_model_annual.jl")
 include("mcs_annual.jl")
 
-# get/define model, with default settings (i.e. NDCs scenario, permafrost, no sea-ice, use_page09damages)
-# m = getpage()
-# or alternatively, with different configurations:
-# m = getpage("2 degC Target", true, true)
-# m = getpage("NDCs", true, false)
-
-# scenario = "RCP2.6 & SSP1"
-# scenario = "RCP4.5 & SSP2"
-# scenario = "RCP8.5 & SSP5"
-
-for scenario in ["RCP2.6 & SSP1", "RCP4.5 & SSP2", "RCP8.5 & SSP5"]
+for scenario in ["1.5 degC Target", "2 degC Target", "RCP2.6 & SSP1", "RCP4.5 & SSP2", "RCP8.5 & SSP5"]
     model = "PAGE-ANN"
+    # define model, default settings: getpage(NDCs scenario, permafrost, no sea-ice, no page09damages)
     m = getpage(scenario, true, true)
     # run model
     run(m)
 
-    # get the social cost of carbon
-    scc = compute_scc(m, year=2020)
-    println(scc)
-
     # open up Explorer UI, for visual exploration of the variables
     # explore(m)
 
-
     samplesize = 50000
-    # get the social cost of carbon for the Monte Carlo simulations, for selected quantiles.
-    # sccs = compute_scc_mcs(m, 5000, year=2020)
-    # sccobs = [quantile(sccs, [.05, .25, .5, .75, .95]); mean(sccs)]
-    # println(sccobs)
-
-
     # do general monte carlo simulation
     do_monte_carlo_runs(samplesize, scenario, joinpath(@__DIR__, "../output", scenario, model))
 end
--- src/run_pagevar.jl
+++ src/run_pagevar.jl
@@ -1,5 +1,6 @@
 using Mimi
 
+# specify model settings
 function set_globalbools()
     global use_variability = true
     global use_annual = true
@@ -20,43 +21,20 @@ end
 set_globalbools()
 
 #run main_model file
-
-# get/define model, with default settings (i.e. NDCs scenario, permafrost, no sea-ice, use_page09damages)
-# m = getpage()
-# or alternatively, with different configurations:
-# m = getpage("2 degC Target", true, true)
-# m = getpage("RCP4.5 & SSP2", true, false)
-# scenario = "RCP2.6 & SSP1"
-# scenario = "RCP4.5 & SSP2"
-# scenario = "RCP8.5 & SSP5"
-
-for scenario in ["RCP2.6 & SSP1", "RCP4.5 & SSP2", "RCP8.5 & SSP5"]
 include("main_model_annual.jl")
 include("mcs_annual.jl")
+
+for scenario in ["1.5 degC Target", "2 degC Target", "RCP2.6 & SSP1", "RCP4.5 & SSP2", "RCP8.5 & SSP5"]
     model = "PAGE-VAR"
+    # define model, default settings: getpage(NDCs scenario, permafrost, no sea-ice, no page09damages)
     m = getpage(scenario, true, true)
     # run model
     run(m)
 
-    # get the social cost of carbon
-    scc = compute_scc(m, year=2020)
-    println(scc)
-
     # open up Explorer UI, for visual exploration of the variables
     # explore(m)
 
-
     samplesize = 50000
-    # get the social cost of carbon for the Monte Carlo simulations, for selected quantiles.
-    # ad-hoc hack to run Monte Carlo simulation for stochastic model.
-    # sccs = zeros(samplesize)
-    # for i in range(1, samplesize)
-    #     sccs[i] = mean(compute_scc_mcs(m, 1, year=2020, pulse_size = 100000.))
-    # end
-    # sccobs = [quantile(sccs, [.05, .25, .5, .75, .95]); mean(sccs)]
-    # println(sccobs)
-
-
     # do general monte carlo simulation
     do_monte_carlo_runs(samplesize, scenario, joinpath(@__DIR__, "../output", scenario, model))
 end
