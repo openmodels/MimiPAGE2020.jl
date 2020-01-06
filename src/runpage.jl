70ef0dac76ab26c676cb523213d334bbc2446f7d
jkikstra <46967503+jkikstra@users.noreply.github.com>
2020-01-06 21:08:56 +0100

    add runfiles to ease running PAGE-ANN and/or PAGE-VAR

--- src/runpage.jl
+++ src/runpage.jl
@@ -13,8 +13,10 @@ end
 function runpage()
     options = "
     Choose model version to run:
-    - 1 PAGE-ICE
-    - 2 PAGE-ICE with Growth Effects
+    - 1 PAGE-ICE (10 timestep)
+    - 2 PAGE-ICE with Growth Effects (10 timestep)
+    - 3 PAGE-ICE (annual)
+    - 4 PAGE-ICE with Interannual Temperature Variability (annual)
 
     Input number:
     "
@@ -22,10 +24,16 @@ function runpage()
     version = Input(options)
 
 
-    if isequal(version, "1") || isequal(version, "PAGE-ICE")
+    if isequal(version, "1") || isequal(version, "PAGE-ICE (10 timestep)")
         include("runmodel.jl")
-    elseif isequal(version, "2") || isequal(version, "PAGE-ICE with Growth Effects")
+    elseif isequal(version, "2") || isequal(version, "PAGE-ICE with Growth Effects (10 timestep)")
         include("runmodel_growth.jl")
+    elseif isequal(version, "3") || isequal(version, "PAGE-ICE (annual)")
+        include("runmodel_annual.jl")
+    elseif isequal(version, "4") || isequal(version, "PAGE-ICE with Interannual Temperature Variability (annual)")
+        include("runmodel_variability")
+    # elseif isequal(version, "5") || isequal(version, "PAGE-ICE with Interannual Temperature Variability with Autoregression (annual)")
+    #     include("runmodel_ARvariability")
     else
         println("WARNING: No valid model input provided. Please provide a valid model choice")
         runpage()
