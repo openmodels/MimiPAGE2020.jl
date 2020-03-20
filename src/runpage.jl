179d5c74101139f884dab6488c8d03f7ca00b8c4
James Rising <jarising@gmail.com>
2020-03-20 02:29:33 -0700

    don't default

--- src/runpage.jl
+++ src/runpage.jl
@@ -24,7 +24,7 @@ function runpage()
     Input number:
     "
 
-    version = "6" #Input(options)
+    version = Input(options)
 
 
     if isequal(version, "1") || isequal(version, "PAGE-ICE (10 timestep)")
