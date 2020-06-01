093ba6e1b95662e62340109665cf3c87d21ce492
James Rising <jarising@gmail.com>
2020-06-01 11:24:16 +0100

    roll back change

--- src/components/CO2emissions.jl
+++ src/components/CO2emissions.jl
@@ -5,8 +5,9 @@
     e0_baselineCO2emissions=Parameter(index=[region],unit="Mtonne/year")
     e_regionalCO2emissions=Variable(index=[time,region],unit="Mtonne/year")
     er_CO2emissionsgrowth=Parameter(index=[time,region],unit="%")
-
+    
     function run_timestep(p, v, d, t)
+
         #eq.4 in Hope (2006) - regional CO2 emissions as % change from baseline
         for r in d.region
             v.e_regionalCO2emissions[t,r]=p.er_CO2emissionsgrowth[t,r]*p.e0_baselineCO2emissions[r]/100
