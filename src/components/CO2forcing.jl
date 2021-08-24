13571027befbd10693dfa2656ac6cf52f71bc562
James Rising <jarising@gmail.com>
2021-08-24 09:39:58 -0400

    allow lower co2

--- src/components/CO2forcing.jl
+++ src/components/CO2forcing.jl
@@ -8,10 +8,6 @@
     function run_timestep(p, v, d, t)
 
         # eq.13 in Hope 2006
-	if p.c_CO2concentration[t] < p.c0_baseCO2conc
-	    v.f_CO2forcing[t] = p.f0_CO2baseforcing
-	else
-            v.f_CO2forcing[t] = p.f0_CO2baseforcing + p.fslope_CO2forcingslope * log(p.c_CO2concentration[t] / p.c0_baseCO2conc)
-	end
+        v.f_CO2forcing[t] = p.f0_CO2baseforcing + p.fslope_CO2forcingslope * log(p.c_CO2concentration[t] / p.c0_baseCO2conc)
     end
 end
